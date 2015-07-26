b7c0b33475773ab1c689562216d1487ecbe62c84
specifi <philip.specifi@gmail.com>
2015-07-26 13:48:05 +0800

    Bring back pagination to 10

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all.order("created_at DESC").page(params[:page])
+    @posts = Post.all.order("created_at DESC").page(params[:page]).per(10)
   end
 end
